52bcb16c80228996dacc838c4f1baf49834d196b
Patrik Oldsberg <poldsberg@gmail.com>
2022-03-02 12:29:24 +0100

    cli: remove deprecated index.html templating variables
    Signed-off-by: Patrik Oldsberg <poldsberg@gmail.com>

--- packages/cli/src/lib/bundler/config.ts
+++ packages/cli/src/lib/bundler/config.ts
@@ -118,43 +118,11 @@ export async function createConfig(
     }),
   );
 
-  const appParamDeprecationMsg = chalk.red(
-    'DEPRECATION WARNING: using `app.<key>` in the index.html template is deprecated, use `config.getString("app.<key>")` instead.',
-  );
   plugins.push(
     new HtmlWebpackPlugin({
       template: paths.targetHtml,
       templateParameters: {
         publicPath: validBaseUrl.pathname.replace(/\/$/, ''),
-        app: {
-          get title() {
-            console.warn(appParamDeprecationMsg);
-            return frontendConfig.getString('app.title');
-          },
-          get baseUrl() {
-            console.warn(appParamDeprecationMsg);
-            return validBaseUrl.href;
-          },
-          get googleAnalyticsTrackingId() {
-            console.warn(appParamDeprecationMsg);
-            return frontendConfig.getOptionalString(
-              'app.googleAnalyticsTrackingId',
-            );
-          },
-          get datadogRum() {
-            console.warn(appParamDeprecationMsg);
-            return {
-              env: frontendConfig.getOptionalString('app.datadogRum.env'),
-              clientToken: frontendConfig.getOptionalString(
-                'app.datadogRum.clientToken',
-              ),
-              applicationId: frontendConfig.getOptionalString(
-                'app.datadogRum.applicationId',
-              ),
-              site: frontendConfig.getOptionalString('app.datadogRum.site'),
-            };
-          },
-        },
         config: frontendConfig,
       },
     }),
